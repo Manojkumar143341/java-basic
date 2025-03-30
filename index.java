9d1db5c0b6536c2c1acc81c69553a8abc706aff3
Manoj kumar <manoj166174@gmail.com>
2025-03-30 19:15:08 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("manojkumar");
+        int age = 10;
+        if(age==10){
+            System.out.println("my age is ten");
+        }
+        else{
+             System.out.println("my age is not ten");
+        }
+    }
+}
